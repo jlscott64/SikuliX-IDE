1127fd9f93fa5311fa078fe78c6cb446841909e2
Raimund Hocke <rmhdevelop@me.com>
2013-07-05 09:28:30 +0200

    started new preview window

---
+++ src/main/java/org/sikuli/ide/PreviewWindow.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sikuli.ide;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.HashSet;
+import java.util.Set;
+import org.sikuli.script.OverlayTransparentWindow;
+import org.sikuli.script.ScreenHighlighter;
+
+/**
+ *
+ * @author rhocke
+ */
+public class PreviewWindow extends OverlayTransparentWindow implements MouseListener {
+
+  public PreviewWindow() {
+   
+  }
+
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+}
